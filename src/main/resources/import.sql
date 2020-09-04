5f4646562f1193e0bc1772ea3a2b57d22319be15
EL KOTB ZAKARIA <EL KOTB ZAKARIA@192.168.1.118>
2020-09-04 16:38:03 +0100

    update sql script

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,28 +1,28 @@
 insert into biller (biller_name, biller_code, biller_description, logo_path, website, category) values ('INWI', '0001', 'INWI Recharge', 'http://localhost:8080/inwi-logo.jpg','https://inwi.com','2');
 insert into debt (debt_code, debt_name, active, biller_id) values ('01','Recharge Téléphonique',true,1);
-insert into form_field (field_name, field_type, field_format, min_size, max_size, contrainte) values ('Numéro de ligne à recharger','text','chaine',4,15,1);
+insert into form_field (field_name, field_type, field_format, min_size, max_size, contrainte) values ('Numéro de ligne à recharger','text','entier',4,15,1);
 insert into form_field (field_name, field_type, list_vals, field_format, min_size, max_size, contrainte) values ('Montant','select','10;20;30;40;50;100;150;300;500','chaine',4,15,1);
 insert into debt_form_fields (debt_id, form_fields_id) values (1,1),(1,2);
 
 insert into biller (biller_name, biller_code, biller_description, logo_path, website, category) values ('ORANGE', '0002', 'Orange Recharge', 'http://localhost:8080/orange-logo.png','https://orange.com','2');
 insert into debt (debt_code, debt_name, active, biller_id) values ('01','Recharge Téléphonique',true,2);
-insert into form_field (field_name, field_type, field_format, min_size, max_size, contrainte) values ('Numéro de ligne à recharger','text','chaine',4,15,1);
+insert into form_field (field_name, field_type, field_format, min_size, max_size, contrainte) values ('Numéro de ligne à recharger','text','entier',4,15,1);
 insert into form_field (field_name, field_type, list_vals, field_format, min_size, max_size, contrainte) values ('Montant','select','10;20;25;30;45;50;100;150;300;500','chaine',4,15,1);
 insert into debt_form_fields (debt_id, form_fields_id) values (2,3),(2,4);
 
 insert into biller (biller_name, biller_code, biller_description, logo_path, website, category) values ('MAROC TELECOM', '0003', 'IAM Recharge', 'http://localhost:8080/maroc-telecom-logo.jpg','https://maroctelecom.com','2');
 insert into debt (debt_code, debt_name, active, biller_id) values ('01','Recharge Téléphonique',false,3);
-insert into form_field (field_name, field_type, field_format, min_size, max_size, contrainte) values ('Numéro de ligne à recharger','text','chaine',4,15,1);
+insert into form_field (field_name, field_type, field_format, min_size, max_size, contrainte) values ('Numéro de ligne à recharger','text','entier',4,15,1);
 insert into form_field (field_name, field_type, list_vals, field_format, min_size, max_size, contrainte) values ('Montant','select','10;20;25;30;45;50;100;150;300;500','chaine',4,15,1);
 insert into debt_form_fields (debt_id, form_fields_id) values (3,5),(3,6);
 
 insert into biller (biller_name, biller_code, biller_description, logo_path, website, category) values ('MAROC TELECOM', '0004', 'IAM Facture', 'http://localhost:8080/maroc-telecom-logo.jpg','https://maroctelecom.com','2');
 insert into debt (debt_code, debt_name, active, biller_id) values ('01','Facture Fixe',true,4);
-insert into form_field (field_name, field_type, field_format, min_size, max_size, contrainte) values ('Numéro de téléphone Fixe','text','chaine',4,15,1);
+insert into form_field (field_name, field_type, field_format, min_size, max_size, contrainte) values ('Numéro de téléphone Fixe','text','entier',4,15,1);
 insert into form_field (field_name, field_type, field_format, min_size, max_size, contrainte) values ('Code Confidentiel','text','chaine',5,5,1);
 insert into debt_form_fields (debt_id, form_fields_id) values (4,7),(4,8);
 insert into debt (debt_code, debt_name, active, biller_id) values ('02','Facture Mobile',true,4);
-insert into form_field (field_name, field_type, field_format, min_size, max_size, contrainte) values ('Numéro de téléphone Mobile','text','chaine',4,15,1);
+insert into form_field (field_name, field_type, field_format, min_size, max_size, contrainte) values ('Numéro de téléphone Mobile','text','entier',4,15,1);
 insert into form_field (field_name, field_type, field_format, min_size, max_size, contrainte) values ('Code Fidélio','text','chaine',5,5,1);
 insert into debt_form_fields (debt_id, form_fields_id) values (5,9),(5,10);
 insert into debt (debt_code, debt_name, active, biller_id) values ('03','Facture Internet',true,4);
@@ -31,7 +31,7 @@ insert into debt_form_fields (debt_id, form_fields_id) values (6,11);
 
 insert into biller (biller_name, biller_code, biller_description, logo_path, website, category) values ('ORANGE', '0005', 'ORANGE Facture', 'http://localhost:8080/orange-logo.png','https://orange.com','2');
 insert into debt (debt_code, debt_name, active, biller_id) values ('01','Facture Particuliers',true,5);
-insert into form_field (field_name, field_type, field_format, min_size, max_size, contrainte) values ('Numéro de Ligne','text','chaine',4,15,1);
+insert into form_field (field_name, field_type, field_format, min_size, max_size, contrainte) values ('Numéro de Ligne','text','entier',4,15,1);
 insert into debt_form_fields (debt_id, form_fields_id) values (7,12);
 insert into debt (debt_code, debt_name, active, biller_id) values ('02','Facture Entreprises',true,5);
 insert into form_field (field_name, field_type, field_format, min_size, max_size, contrainte) values ('RC','text','chaine',4,15,1);
@@ -54,12 +54,12 @@ insert into debt_form_fields (debt_id, form_fields_id) values (11,17);
 insert into biller (biller_name, biller_code, biller_description, logo_path, website, category) values ('DGI', '0008', 'Paiement Impots', 'http://localhost:8080/dgi-logo.jpg','https://dgi.com','4');
 insert into debt (debt_code, debt_name, active, biller_id) values ('01','Frais Immatriculation',true,8);
 insert into form_field (field_name, field_type, field_format, min_size, max_size, contrainte) values ('Référence de paiment','text','chaine',4,15,1);
-insert into form_field (field_name, field_type, field_format, min_size, max_size, contrainte) values ('Montant avant la virgule','text','chaine',4,15,1);
+insert into form_field (field_name, field_type, field_format, min_size, max_size, contrainte) values ('Montant avant la virgule','text','entier',4,15,1);
 insert into debt_form_fields (debt_id, form_fields_id) values (12,18),(12,19);
 insert into debt (debt_code, debt_name, active, biller_id) values ('02','DGI E-TIMBRE',true,8);
 insert into form_field (field_name, field_type, list_vals, field_format, min_size, max_size, contrainte) values ('Rechercher par','select','Référence E-Timbre','chaine',4,15,1);
 insert into form_field (field_name, field_type, field_format, min_size, max_size, contrainte) values ('Référence E-Timbre','text','chaine',4,15,1);
-insert into form_field (field_name, field_type, field_format, min_size, max_size, contrainte) values ('Montant','text','chaine',4,15,1);
+insert into form_field (field_name, field_type, field_format, min_size, max_size, contrainte) values ('Montant','text','Reel',4,15,1);
 insert into debt_form_fields (debt_id, form_fields_id) values (13,20),(13,21),(13,22);
 
 insert into biller (biller_name, biller_code, biller_description, logo_path, website, category) values ('RAM', '0009', 'Achat Ticket Avion', 'http://localhost:8080/ram-logo.png','https://ram.com','3');
